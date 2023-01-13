6a02be44e39a9f3707bb29074deb834192704820
Tristan Young <tyoung22@turing.edu>
2023-01-12 18:11:18 -0800

    Run the keys of foods

--- objects.js
+++ objects.js
@@ -13,7 +13,8 @@ foods = {
 
 // Write code that prints all of the 'keys' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods))
+
 
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
@@ -61,7 +62,7 @@ foods = {
 // Check out the following example of an array of Instagram posts:
 
 
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
+// posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
 // An Array of Objects is probably more realistic. Objects are a data type that's great
@@ -69,25 +70,25 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 // that relate to each individual Instagram post:
 
 
-posts = [
-  {
-    imageSrc: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  }, 
-  {
-    imageSrc: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-];
+// posts = [
+//   {
+//     imageSrc: "./images/beach.png",
+//     caption: "At the beach with my besties",
+//     timestamp: "4:37 PM August 13, 2019",
+//     number_likes: 0,
+//     comments: []
+//   }, 
+//   {
+//     imageSrc: "./images/holiday-party.png",
+//     caption: "What a great holiday party omg",
+//     timestamp: "11:37 PM December 31, 2019",
+//     number_likes: 13,
+//     comments: []
+//   }
+// ];
 
-console.log(posts);
-console.log(posts[0]);
+// console.log(posts);
+// console.log(posts[0]);
 
 
 // The code snippet above shows an Array with 2 elements. Each element in the Array is a 
